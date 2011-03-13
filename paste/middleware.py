b9d426f54ffcd1d85bbbe0c837e22d2376ea670c
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2011-03-13 21:43:27 +0200

    beautifulsoup middleware

---
+++ paste/middleware.py
@@ -0,0 +1,9 @@
+import settings
+from BeautifulSoup import BeautifulSoup
+
+class PrettifyMiddleware(object):
+    """HTML code prettification middleware."""
+    def process_response(self, request, response):
+        if settings.DEBUG and response['Content-Type'].split(';', 1)[0] == 'text/html':
+            response.content = BeautifulSoup(response.content).prettify()
+        return response
